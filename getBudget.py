a19241b0dc900a8920e94fff22dc8f7f64b22a2e
Darley <tiusdiux.com@gmail.com>
2020-10-03 15:10:43 -0500

    Modified Test 2

--- getBudget.py
+++ getBudget.py
@@ -1,7 +1,10 @@
 def getTotalBudget(people):
-    pass
     '''Your amazing code here'''
-    totalBudget = 0
-    for i in people:
-        totalBudget += i['budget']
-    return totalBudget
+    # totalBudget = 0
+    # for i in people:
+        # totalBudget += i['budget']
+
+    # with list comprehension
+    list_1 = [i['budget'] for i in people]
+
+    return sum(list_1)
